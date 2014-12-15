ef5c559549ed79ca0331d7a1b059f95f407870d1
Dan Grabowski <dan.grabowski@gmail.com>
2014-12-15 13:16:34 -0500

    Finished initial fizzbuzz exercise.

--- fizzBuzz/src/main/java/FizzBuzz.java
+++ fizzBuzz/src/main/java/FizzBuzz.java
@@ -1,7 +1,30 @@
 public class FizzBuzz {
 
     public String process(Integer inputValue) {
-       return null;
+        checkInputConditions(inputValue);
+        if (isMultipleOf3(inputValue) && isMultipleOf5(inputValue)) {
+            return "FizzBuzz";
+        } else if (isMultipleOf3(inputValue)) {
+            return "Fizz";
+        } else if (isMultipleOf5(inputValue)) {
+            return "Buzz";
+        } else {
+            return inputValue.toString();
+        }
     }
 
+    private void checkInputConditions(Integer inputValue) {
+        assert(inputValue != null);
+        if (inputValue < 1 || inputValue > 100) {
+            throw new IllegalArgumentException("Input must be between 1 and 100, inclusive.");
+        }
+    }
+
+    private boolean isMultipleOf5(Integer inputValue) {
+        return inputValue % 5 == 0;
+    }
+
+    private boolean isMultipleOf3(Integer inputValue) {
+        return inputValue % 3 == 0;
+    }
 }
--- fizzBuzz/src/test/java/FizzBuzzTest.java
+++ fizzBuzz/src/test/java/FizzBuzzTest.java
@@ -1,14 +1,72 @@
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class FizzBuzzTest {
 
 	@Test
     public void shouldReturn1for1() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
-        String actualReturnValue = fizzBuzz.process(1);
-        String expectedReturnValue = "1";
-        assertEquals(expectedReturnValue, actualReturnValue);
+        checkFizzBuzz("1", 1);
+    }
+
+    @Test
+    public void shouldReturn1for2() {
+        checkFizzBuzz("2", 2);
+    }
+
+    @Test
+    public void shouldReturnFizzForMultiplesOf3() {
+        checkFizzBuzz("Fizz", 3);
+        checkFizzBuzz("Fizz", 6);
+        checkFizzBuzz("Fizz", 9);
+    }
+
+    @Test
+    public void shouldReturnBuzzForMultiplesOf5() {
+        checkFizzBuzz("Buzz", 5);
+        checkFizzBuzz("Buzz", 10);
+        checkFizzBuzz("Buzz", 20);
+    }
+
+    @Test
+    public void shouldReturnFizzBuzzForMultiplesOf3And5() {
+        checkFizzBuzz("FizzBuzz", 15);
+        checkFizzBuzz("FizzBuzz", 30);
+        checkFizzBuzz("FizzBuzz", 45);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void shouldFailAssertionWithNullInput() {
+        new FizzBuzz().process(null);
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionForValuesLessThan1() {
+        checkForIllegalArgumentException(0);
+        checkForIllegalArgumentException(-1);
+        checkForIllegalArgumentException(Integer.MIN_VALUE);
+    }
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionForValuesGreaterThan100() {
+        checkForIllegalArgumentException(101);
+        checkForIllegalArgumentException(102);
+        checkForIllegalArgumentException(Integer.MAX_VALUE);
+    }
+
+    private void checkForIllegalArgumentException(int input) {
+        try {
+            new FizzBuzz().process(input);
+            fail("Expected IllegalArgumentException.");
+        } catch (Exception e) {
+            // expected
+        }
+    }
+
+    private void checkFizzBuzz(String expectedReturnValue, Integer input) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String actualReturnValue = fizzBuzz.process(input);
+        assertEquals(String.format("Failed for input %s.", input), expectedReturnValue, actualReturnValue);
     }
 }
