97dd9d46feef3c51b121accbae08d5bde827f469
Dan Grabowski <dan.grabowski@gmail.com>
2014-12-16 16:10:17 -0500

    In progress work on gilded rose

--- gildedRose/src/test/java/GildedRoseTest.java
+++ gildedRose/src/test/java/GildedRoseTest.java
@@ -1,12 +1,140 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 
 public class GildedRoseTest {
 
+	private Item sulfuras;
+	private Item vest;
+	private Item concertTicket;
+
+	@Before
+	public void setUp() throws Exception {
+		sulfuras = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+		vest = new Item("+5 Dexterity Vest", 10, 20);
+		concertTicket = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20);
+	}
+
 	@Test
-	public void testTheTruth() {
-		assertTrue(true);
+	public void sulfurasKeepsQuality80AfterUpdate() throws Exception {
+		sulfuras.setSellIn(1);
+		setupItems(sulfuras);
+
+		GildedRose.updateQuality();
+		assertEquals(80, sulfuras.getQuality());
+	}
+
+	@Test
+	public void updatingQualityShouldNotAddOrRemoveItems() throws Exception {
+		setupItems(sulfuras);
+
+		GildedRose.updateQuality();
+		assertEquals(1, GildedRose.getItems().size());
+		assertSame(sulfuras, GildedRose.getItems().get(0));
+	}
+
+	@Test
+	public void updatingQualityShouldDecreaseSellInBy1() throws Exception {
+		setupItems(vest);
+
+		GildedRose.updateQuality();
+		assertEquals(9, vest.getSellIn());
+	}
+
+	@Test
+	public void updatingQualityShouldNotDecreaseSellInForSulfuras() throws Exception {
+		setupItems(sulfuras);
+
+		GildedRose.updateQuality();
+		assertEquals(0, sulfuras.getSellIn());
+	}
+
+	@Test
+	public void itemQualityDecreasesBy1BeforeSellInDate() throws Exception {
+		setupItems(vest);
+
+		GildedRose.updateQuality();
+		assertEquals(19, this.vest.getQuality());
+	}
+
+	@Test
+	public void itemQualityDecreasesBy2AfterSellInDate() throws Exception {
+		vest.setSellIn(0);
+		setupItems(vest);
+
+		GildedRose.updateQuality();
+		assertEquals(18, this.vest.getQuality());
+	}
+
+	@Test
+	public void itemQualityCannotDecreaseBellow0() throws Exception {
+		vest.setQuality(0);
+		setupItems(vest);
+
+		GildedRose.updateQuality();
+		assertEquals(0, vest.getQuality());
+	}
+
+	@Test
+	public void agedBrieIncreasesInQuality() throws Exception {
+		Item brie = new Item("Aged Brie", 2, 0);
+		setupItems(brie);
+
+		GildedRose.updateQuality();
+		assertEquals(1, brie.getQuality());
+	}
+
+	@Test
+	public void agedBrieIncreasesInQualityPastSellByDate() throws Exception {
+		Item brie = new Item("Aged Brie", 0, 0);
+		Item olderBrie = new Item("Aged Brie", -1, 2);
+		setupItems(brie, olderBrie);
+
+		GildedRose.updateQuality();
+		assertEquals(2, brie.getQuality());
+		assertEquals(4, olderBrie.getQuality());
+	}
+
+	@Test
+	public void concertTicketsIncreaseInQualityMoreThan10DaysLeft() throws Exception {
+		setupItems(concertTicket);
+
+		GildedRose.updateQuality();
+		assertEquals(21, concertTicket.getQuality());
+	}
+
+	@Test
+	public void concertTicketsIncreaseInQualityBetween6and10DaysLeft() throws Exception {
+		concertTicket.setSellIn(10);
+		setupItems(concertTicket);
+
+		GildedRose.updateQuality();
+		assertEquals(22, concertTicket.getQuality());
+	}
+
+	@Test
+	public void concertTicketsIncreaseInQualityBetween5and1DaysLeft() throws Exception {
+		concertTicket.setSellIn(5);
+		setupItems(concertTicket);
+
+		GildedRose.updateQuality();
+		assertEquals(23, concertTicket.getQuality());
+	}
+
+	@Test
+	public void concertTicketsIncreaseInQuality0DaysLeft() throws Exception {
+		concertTicket.setSellIn(0);
+		setupItems(concertTicket);
+
+		GildedRose.updateQuality();
+		assertEquals(0, concertTicket.getQuality());
+	}
+
+	private void setupItems(Item... items) {
+		GildedRose.setItems(Arrays.asList(items));
 	}
 }
--- romanNumerals/src/main/java/RomanNumeralConverter.java
+++ romanNumerals/src/main/java/RomanNumeralConverter.java
@@ -1,8 +1,25 @@
-import java.util.Map;
+import java.util.Comparator;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class RomanNumeralConverter {
 
+    private final static SortedMap<Integer, Character> romanNumeralValues = new TreeMap<>(new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return -1 * o1.compareTo(o2);
+        }
+    });
 
+    {
+        romanNumeralValues.put(1000, 'M');
+        romanNumeralValues.put(500, 'D');
+        romanNumeralValues.put(100, 'C');
+        romanNumeralValues.put(50, 'L');
+        romanNumeralValues.put(10, 'X');
+        romanNumeralValues.put(5, 'V');
+        romanNumeralValues.put(1, 'I');
+    }
 
     public static Integer convert(String numeralText) {
         Integer sum = 0;
@@ -25,33 +42,43 @@ public class RomanNumeralConverter {
     public static String convert(Integer integer) {
         StringBuilder romanNumeral = new StringBuilder();
         while (integer > 0) {
-            if (integer >= 1000) {
-                romanNumeral.append("M");
-                integer -= 1000;
-            } else if (integer >= 500) {
-                romanNumeral.append("D");
-                integer -= 500;
-            } else if (integer >= 100) {
-                romanNumeral.append("C");
-                integer -= 100;
-            } else if (integer >= 50) {
-                romanNumeral.append("L");
-                integer -= 50;
-            } else if (integer >= 10) {
-                romanNumeral.append("X");
-                integer -= 10;
-            } else if (integer >= 5) {
-                romanNumeral.append("V");
-                integer -= 5;
-            } else if (integer >= 1) {
-                if (integer == 4) {
-                    romanNumeral.append("IV");
-                    integer -= 4;
-                } else {
-                    romanNumeral.append("I");
-                    integer--;
+            for (Integer value : romanNumeralValues.keySet()) {
+                if (integer >= value) {
+                    romanNumeral.append(romanNumeralValues.get(value));
+                    integer -= value;
+                    break;
                 }
             }
+
+
+
+//            if (integer >= 1000) {
+//                romanNumeral.append("M");
+//                integer -= 1000;
+//            } else if (integer >= 500) {
+//                romanNumeral.append("D");
+//                integer -= 500;
+//            } else if (integer >= 100) {
+//                romanNumeral.append("C");
+//                integer -= 100;
+//            } else if (integer >= 50) {
+//                romanNumeral.append("L");
+//                integer -= 50;
+//            } else if (integer >= 10) {
+//                romanNumeral.append("X");
+//                integer -= 10;
+//            } else if (integer >= 5) {
+//                romanNumeral.append("V");
+//                integer -= 5;
+//            } else if (integer >= 1) {
+//                if (integer == 4) {
+//                    romanNumeral.append("IV");
+//                    integer -= 4;
+//                } else {
+//                    romanNumeral.append("I");
+//                    integer--;
+//                }
+//            }
         }
 
         return romanNumeral.toString();
